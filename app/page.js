377a5760ed6fb866b1c91e045edd7871da3f06b0
phuwadech <aofphuwadech@gmail.com>
2024-07-18 16:04:03 +0700

    add icon

--- app/page.js
+++ app/page.js
@@ -217,8 +217,15 @@ const Home = () => {
                     <Col xs={12} md={8} className={''}>
                         <div className="text-center">
                             <img src="https://www.wordgames.com/images/logo.png" alt="logo" width={300}/>
-                            <h1 className="mb-4">เกมทายคำจากภาพ</h1>
-                            <Button variant="primary" size="lg" onClick={handleStartGame}>เริ่มเกม</Button>
+                            <h1 className="mb-4" style={{fontSize: 42}}>เกมทายคำจากภาพ</h1>
+                            <Button variant="primary" size="lg" onClick={handleStartGame}>
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 640 512">
+                                    <path fill="#ffffff"
+                                          d="M192 64C86 64 0 150 0 256S86 448 192 448l256 0c106 0 192-86 192-192s-86-192-192-192L192 64zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24l0 32 32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0 0 32c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-32-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l32 0 0-32z"/>
+                                </svg>
+                                เริ่มเกม
+                            </Button>
                         </div>
                     </Col>
                 </Row>
